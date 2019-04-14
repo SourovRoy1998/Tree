b94c0a2eddd8bd5460016c8402dc12adea8ea73a
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-04-14 09:39:43 +0530

    Create divisor-game.cpp

---
+++ divisor-game.cpp
@@ -0,0 +1,10 @@
+//https://leetcode.com/contest/weekly-contest-132/submissions/detail/222248041/
+
+class Solution {
+public:
+    bool divisorGame(int N) {
+        if(N%2)
+            return false;
+        return true;
+    }
+};
